16d97a8bcfe5bd17b4dfe9924f49eb713f55beab
Dhairya <dhairya.ostwal@gmail.com>
2021-10-10 00:19:15 +0530

    Added Recommendation Data

---
+++ src/data/RecommendationData.js
@@ -0,0 +1,12 @@
+export const RecommendationData = [
+  {
+    name: 'Puma Slippers',
+    price: '50',
+    url: 'https://cdn.shopify.com/s/files/1/2428/5565/products/Neemans-PebbleGrey-Recycled-Rubber-Eco-Slides-FrontLeftSideTiltLogoAir-Comfortable-Slides_700x.progressive.jpg?v=1630058449',
+  },
+  {
+    name: 'Leelavati Bags for Women',
+    price: '350',
+    url: 'https://fossil.scene7.com/is/image/FossilPartners/ZB1615200_main?$sfcc_fos_large$',
+  },
+];
